e18f0eeecd452ede147eff2284e8141cf9e436c9
Chandrakant Birhade <chandrakant.birhade@clariontech.com>
2022-06-20 10:38:26 +0530

    test changes

--- test/TestClass.java
+++ test/TestClass.java
@@ -39,7 +39,7 @@ public static void duplicateCharCount()
 			if(input[i] == 's')
 			{
 				count++;
-			}
+			} 
 	}
 	
 	System.out.println("The repeated Char count is : " + count);
@@ -265,4 +265,7 @@ Finally, display the character and their corresponding frequencies by iterating 
 * Pull and rebase commands how it works --- multiple branching conflict how to resolve it
 * code changes updated as records point of view
 */
-
+/// https://www.geeksforgeeks.org/check-if-given-string-contains-all-the-digits/?ref=lbp badiya website for learning and analysis point of veiw https://www.geeksforgeeks.org/encapsulation-in-java/?ref=lbp
+//https://www.geeksforgeeks.org/collections-in-java-2/?ref=lbp - imp
+//https://www.geeksforgeeks.org/arrays-in-java/?ref=lbp - imp
+//https://www.geeksforgeeks.org/strings-in-java/?ref=lbp -imp
