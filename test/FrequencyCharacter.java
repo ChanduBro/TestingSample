5b6ff3012313f9b0ce73099cfb4b7524859b5938
Chandrakant Birhade <chandrakant.birhade@clariontech.com>
2022-06-28 13:53:03 +0530

    changes

--- test/FrequencyCharacter.java
+++ test/FrequencyCharacter.java
@@ -2,18 +2,22 @@ package test;
 
 public class FrequencyCharacter     
 {    
-     public static void main(String[] args) {    
+     public static void main(String[] args)
+     {    
         String str = "picture perfect";    
         int[] freq = new int[str.length()];    
         int i, j;    
             
-        //Converts given string into character array    
+        // Converts given string into character array    
         char string[] = str.toCharArray();    
             
-        for(i = 0; i <str.length(); i++) {    
+        for(i = 0; i <str.length(); i++) 
+        {    
             freq[i] = 1;    
-            for(j = i+1; j <str.length(); j++) {    
-                if(string[i] == string[j]) {    
+            for(j = i+1; j <str.length(); j++) 
+            {    
+                if(string[i] == string[j]) 
+                {    
                     freq[i]++;    
                         
                     //Set string[j] to 0 to avoid printing visited character    
@@ -24,9 +28,13 @@ public class FrequencyCharacter
             
         //Displays the each character and their corresponding frequency    
         System.out.println("Characters and their corresponding frequencies");    
-        for(i = 0; i <freq.length; i++) {    
+        
+        for(i = 0; i <freq.length; i++) 
+        {    
             if(string[i] != ' ' && string[i] != '0')    
-                System.out.println(string[i] + "-" + freq[i]);    
+            
+            System.out.println(string[i] + "-" + freq[i]);    
+        
         }    
     }    
 }    
